0d0727c4d21d7bd80b067390fbe4d0c5e75536f6
vinukumar <hyzvinukumar@gmail.com>
2023-08-19 14:16:47 +0530

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CountrySeeder::class);
+        $this->call(StateSeeder::class);
+        $this->call(DistrictSeeder::class);
+        $this->call(RoleSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
