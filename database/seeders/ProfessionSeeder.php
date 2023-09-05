3c32a922e8da514765d88551669a3ed0a4925aae
vinukumar <hyzvinukumar@gmail.com>
2023-09-05 09:23:58 +0530

    Seeders

---
+++ database/seeders/ProfessionSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            ['profession_name' => 'Doctor'],
+            ['profession_name' => 'Engineer'],
+            ['profession_name' => 'Others'],
+
+        ];
+        foreach ($data as $item){
+            DB::table('professions')->insert(['religion_name' => $item['profession_name']], $item);
+        }
+    }
+}
